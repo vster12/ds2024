9580d9c3be97c0b8d952013f7d8f21e38cad0887
vster12 <97301713+vster12@users.noreply.github.com>
2024-04-23 22:53:54 +0700

    Update file lab 1

---
+++ Lab 1/client.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#define SIZE 1024
+
+void send_file(FILE *fp, int sockfd){
+    char data[SIZE] = {0};
+    while(fgets(data, SIZE, fp) != NULL) {
+        if (send(sockfd, data, sizeof(data), 0) == -1) {
+            perror("[-]Error in sending file.");
+            exit(1);
+        }
+    bzero(data, SIZE);
+    }
+}
+
+int main(){
+  char *ip = "127.0.0.1";
+  int port = 8080;
+  int e;
+
+  int sockfd;
+  struct sockaddr_in server_addr;
+  FILE *fp;
+  char *filename = "1.txt";
+
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if(sockfd < 0) {
+    perror("[-]Error in socket");
+    exit(1);
+  }
+  printf("[+]Server socket created.\n");
+
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = port;
+  server_addr.sin_addr.s_addr = inet_addr(ip);
+
+  e = connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+  if(e == -1){  
+    perror("[-]Error in connecting");
+    exit(1);
+  }
+  printf("[+]Connected to server.\n");
+
+  fp = fopen(filename, "r");
+  if (fp = NULL){
+    perror("[-]Error in read file");
+    exit(1);
+  }
+
+  send_file(fp, sockfd);
+  printf("[+]File data send successfully\n");
+  close(sockfd);
+  printf("[+]Disconnected from server\n");
+
+  return 0;
+}
